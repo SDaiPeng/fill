2b7aa96076ca3538e7a6d07ad60c40b54fb62e8d
Daipeng <daipeng@easyretailpro.com>
2019-02-22 14:21:16 +0800

    添加测试功能

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"github.com/kataras/iris"
+	"net"
+)
 
-func main(){
-	fmt.Println("hslslslslsllssl")
+const(
+	ip = ""
+	port = 3333
+)
+
+func main() {
+	app := iris.New()
+
+	l, err := net.ListenTCP("tcp", &net.TCPAddr{net.ParseIP(ip),port,""})
+	if err != nil {
+		panic(err)
+	}
+	app.Run(iris.Listener(l))
 }
