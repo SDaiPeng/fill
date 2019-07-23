0965d956a10e83f9e9484de2db1a0d5f4970dcca
Daipeng <daipeng@easyretailpro.com>
2019-07-23 17:47:46 +0800

    websocket学习

--- server/main.go
+++ server/main.go
@@ -1,52 +1,53 @@
 package main
 
 import (
+	"fill/server/wsConn"
 	"fmt"
-	"net"
-	"os"
-	"strings"
+	"github.com/gorilla/websocket"
+	"net/http"
 	"time"
 )
 
-func main() {
-	bindInfo := ":10000"
-	tcpAddr, err := net.ResolveTCPAddr("tcp4", bindInfo)
-	checkError(err)
-	listener, err := net.ListenTCP("tcp", tcpAddr)
-	checkError(err)
-	fmt.Println("等待连接")
-	for {
-		cc, err := listener.Accept()
-		if err != nil {
-			continue
-		}
-		go handleClient(cc)
+var (
+	upGrader = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
 	}
+)
+
+func wsHnadler(w http.ResponseWriter, r *http.Request) {
+	var (
+		data         []byte
+		err          error
+		wsConnection *websocket.Conn
+		connection   *wsConn.Connection
+	)
+	if wsConnection, err = upGrader.Upgrade(w, r, nil); err != nil {
+		return
+	}
+	if connection, err = wsConn.ConnectionInit(wsConnection); err != nil {
+		goto Err
+	}
+	go func() {
+		for {
+			if err =  connection.WriteMessage([]byte("hello world")); err != nil {
+				return
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	for {
+		if data, err = connection.ReadMessage(); err != nil {
+			return
+		}
+		fmt.Println(string(data))
+	}
+Err:
+	connection.Close()
 }
 
-func checkError(err error) {
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-}
-func handleClient(conn net.Conn) {
-	conn.SetReadDeadline(time.Now().Add(3 * time.Minute))
-	request := make([]byte, 1024)
-	defer conn.Close()
-	for {
-		recvLen, err := conn.Read(request)
-		if err != nil {
-			fmt.Println(err)
-			break
-		}
-		if recvLen == 0 {
-			break
-		}
-		recvData := strings.TrimSpace(string(request[:recvLen]))
-		fmt.Println("获取长度 : ", recvLen)
-		fmt.Println("获取数据 : " + recvData)
-		conn.Write([]byte("测试一下BUG\n"))
-		request = make([]byte, 1024)
-	}
+func main() {
+	http.HandleFunc("/ws", wsHnadler)
+	http.ListenAndServe("0.0.0.0:7777", nil)
 }
